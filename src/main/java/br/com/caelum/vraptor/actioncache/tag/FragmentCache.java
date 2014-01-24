b6627db173200f1694fcbeaf3f7c2666cdc60671
Alberto Souza <alots.ssa@gmail.com>
2014-01-24 19:34:56 -0200

    fragmentpage cache

---
+++ src/main/java/br/com/caelum/vraptor/actioncache/tag/FragmentCache.java
@@ -0,0 +1,45 @@
+package br.com.caelum.vraptor.actioncache.tag;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+
+import javax.enterprise.inject.spi.CDI;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+import br.com.caelum.vraptor.actioncache.ActionCache;
+import br.com.caelum.vraptor.actioncache.ActionCacheEntry;
+
+/**
+ * It should keep jsp fragment in cache. 
+ * 
+ * @author Alberto Souza(@asouza)
+ *
+ */
+public class FragmentCache extends SimpleTagSupport {	
+
+	private int duration;
+	private String key;	
+	private static ActionCache actionCache = CDI.current().select(ActionCache.class).get();
+
+	@Override
+	public void doTag() throws JspException, IOException {
+		super.doTag();
+		final StringWriter body = new StringWriter();
+		ActionCacheEntry entry = actionCache.fetch(key);
+		if (entry == null) {
+			getJspBody().invoke(body);
+			actionCache.write(key, new ActionCacheEntry(body.toString(), new HashMap<String, String>()));
+		}
+		getJspContext().getOut().println(actionCache.fetch(key).getResult());
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+}
